4450b74abfa618478a9ccde96ffe1188c84b40ac
ZinyoungChoi <zychoi@devrock.co.kr>
2022-12-23 18:48:11 +0900

    초기화 버튼,input 수정

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,99 +1,22 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from "react";
 import "./Input.css";
-class Input extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value,
-      isInputError: false,
-      isFirstFocus: false,
-    };
 
-    this.setRef = this.setRef.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleFocus = this.handleFocus.bind(this);
-  }
-
-  setRef(ref) {
-    this.ref = ref;
-  }
-
-  handleChange(e) {
-    const { name, onChange } = this.props;
-    console.log(
-      `handleChange() name : ${name} , e.target.value : ${e.target.value}`
-    );
-
-    let checkError = false;
-
-    this.setState({ isInputError: checkError, value: e.target.value });
-
-    onChange(name, e.target.value);
-  }
-
-  handleFocus(e) {
-    const { name, onFocus } = this.props;
-    console.log(
-      `handleFocus() name : ${name} , e.target.value : ${e.target.value}`
-    );
-    if (this.state.isFirstFocus === false) {
-      console.log(
-        `First on focus -> name : ${name} , e.target.value : ${e.target.value}`
-      );
-      this.setState({ isFirstFocus: true });
-    }
-
-    onFocus(name, e.target.value);
-  }
-
-  componentDidMount() {
-    if (this.props.autoFocus) {
-      this.ref.focus();
-    }
-  }
-  
-
-  render() {
-    const { label, name, type, errorMessage } = this.props;
-    return (
-      <div className="input_wrap">
-        <label className="label">
-          {label}
-          <input
-            className="input"
-            id={`input_${name}`}
-            value={this.state.value}
-            type={type}
-            ref={this.setRef}
-            onChange={this.handleChange}
-            onFocus={this.handleFocus}
-          />
-          {this.state.isInputError && errorMessage}
-        </label>
-      </div>
-    );
-  }
+function Input({ type, label, name, value, onChange }) {
+  return (
+    <div className="input_wrap">
+      <label className="label">
+        {label}
+        <input
+          className="input"
+          id={`input_${name}`}
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
 }
-
-Input.propTypes = {
-  label: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  type: PropTypes.oneOf(["text", "password"]),
-  autoFocus: PropTypes.bool,
-  errorMessage: PropTypes.string,
-  onChange: PropTypes.func,
-  onFocus: PropTypes.func,
-};
-
-Input.defaultProps = {
-  value: "",
-  type: "text",
-  autoFocus: false,
-  onChange: () => {},
-  onFocus: () => {},
-};
 
 export default Input;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,17 +4,42 @@ import Button from "../components/Button";
 import "./SignUp.css";
 
 function SignUp() {
+
+  const [value, setValue] = useState({
+    id1: "",
+    pw: "",
+    pwCheck: "",
+    email: "",
+    emailSelect: "",
+  });
+  const {id1, pw, pwCheck, email, emailSelect} = value;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setValue({
+      ...value,
+      [name]: value,
+    });
+  }
+  const onReset = () => {
+    setValue({
+      id1: "",
+      pw: "",
+      pwCheck: "",
+      email: "",
+      emailSelect: "",
+    });
+  };
   return (
     <div className="signup_wrap">
       <h1>회원가입</h1>
       <div className="input_wrap2">
         <div className="input_button">
           <Input
-            className="idcheck"
             label="ID"
             name="id1"
-            autoFocus={true}
             type="text"
+            value={id1}
+            onChange={onChange}
           />
           <Button
             onClick={() => alert("사용중인 아이디입니다")}
@@ -26,20 +51,22 @@ function SignUp() {
           </Button>
         </div>
         <div className="interval">
-          <Input label="PW" name="pw" type="password"  />
+          <Input label="PW" name="pw" type="password" value={pw} onChange={onChange} />
         </div>
         <div className="interval">
           <Input
             label="PW체크"
             name="pwcheck"
             type="password"
+            value={pwCheck}
+            onChange={onChange}
           />
         </div>
         <div className="email_wrap">
-          <Input label="Email" name="mail" type="text" />
+          <Input label="Email" name="email" type="text" value={email} onChange={onChange} />
           <span className="symbols">@</span>
           <select className="select">
-            <option value=" "> </option>
+            <option value={emailSelect}> </option>
             <option value="devrock.co.kr">devrock.co.kr</option>
             <option value="naver.com">naver.com</option>
             <option value="gmail.com">gmail.com</option>
@@ -54,7 +81,7 @@ function SignUp() {
         >
           가입하기
         </Button>
-        <Button background="slategrey">초기화</Button>
+        <Button onClick={onReset} background="slategrey">초기화</Button>
       </div>
     </div>
   );
